2f75290dfa43317bd23d9b9e1571550dc50bc089
Shervin Peterson <iranaghideh71@gmail.com>
2025-12-25 22:45:16 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/studio',
+  //basePath: '/studio',
   assetPrefix: '/studio',
   typescript: {
     ignoreBuildErrors: true,
